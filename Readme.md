cc76874494a988dc52fe064fbeb06d1d19d8cbf2
BabyXinQAQ <1635639396@qq.com>
2019-08-30 22:03:38 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,45 @@
+# 课程选择器
+
+## 配置
+
+| 环境         | 版本          |
+| ------------ | ------------- |
+| python       | 3.7.3         |
+| selenium     | 3.141.0       |
+| chrome       | 76.0.3809.132 |
+| chromedriver | 76.0.3809.126 |
+
+如果你的chrome并非此版本,请在下方网址根据chrome版本下载对应版本的chromedriver
+
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+## 使用
+
+### 修改文件参数
+
+- username
+  用户名(学号)
+- password
+  密码
+- course
+  key 为课程编号, value为所希望选择的课程的选课按钮对应的`xpath`
+
+### 使用教程
+
+1. 修改文件参数.
+   因为使用该脚本需要提前预知课程对应的`xpath`,所以需要在抢课之前进入选课网站将文件参数修改好
+
+2. 在**提前**于选课时间13:00三到四分钟在命令行输入
+
+   ```shell
+   python course_selector.py
+   ```
+
+3. 选到想要的课程后,不要大声张扬,在github上点个**关注**就好。
+   没有选到也不用灰心丧气,因为明天也可能选不到。
+
+### TODO
+
+1. 还希望添加xrld模块，读取excel表格，让调整文件参数更轻松
+
+2. 希望提供一种算法在不使用xpath的情况下保持功能。因为xpath太鸡儿不稳定了，每学期选课都要改。
---
+++ course_selector.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+import time
+
+username = ""
+password = ""
+
+course = dict()
+course['MAE203B'] = '//*[@id="div_201820193000006"]/a'
+course['ME103'] =  '//*[@id="div_201820193000075"]/a'
+
+
+bs = webdriver.Chrome()
+# log in
+url = 'https://jwxt.sustech.edu.cn/jsxsd/framework/xsMain.jsp'
+bs.get(url)
+account = bs.find_element_by_xpath('//*[@id="username"]')
+account.send_keys(username)
+password = bs.find_element_by_xpath('//*[@id="password"]')
+password.send_keys(password)
+btn_login = bs.find_element_by_xpath('//*[@id="fm1"]/section[4]/input[4]')
+btn_login.click()
+# 点击选课中心
+
+btn1 = bs.find_element_by_xpath('/html/body/div[5]/a[1]/div')
+btn1.click()
+# 刷新进入选课
+already = False
+while not already:
+    time.sleep(0.5)
+    try:
+        already = True
+        btn2 = bs.find_element_by_xpath('//*[@id="tbKxkc"]/tbody/tr[2]/td[4]/a')
+        btn2.click()
+
+    except Exception:
+        already = False
+        bs.refresh()
+
+btn3 = bs.find_element_by_xpath('/html/body/div[3]/div[2]/center/input')
+btn3.click()
+
+# course selecting
+all = bs.window_handles
+bs.switch_to.window(all[1])
+btn4 = bs.find_element_by_xpath('//*[@id="topmenu"]/li[6]/a')
+btn4.click()
+
+
+bs.switch_to.frame("mainFrame")
+
+for courseName in course.keys():
+    # 切换iframe
+    text_board = bs.find_element_by_xpath('//*[@id="kcxx"]')
+    text_board.clear()
+    text_board.send_keys(courseName)
+    bs.find_element_by_xpath('/html/body/div[2]/input[5]').click()
+    try:
+        bs.implicitly_wait(20)
+        btn = bs.find_element_by_xpath(course[courseName])
+        btn.click()
+        dig_alert = bs.switch_to.alert
+        time.sleep(0.5)
+        dig_alert.accept()
+        dig_alert = bs.switch_to.alert
+        time.sleep(0.5)
+        dig_alert.accept()
+
+
+    except Exception:
+        print('error')
+        print(Exception)
+
